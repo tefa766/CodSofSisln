87cebe524953e93fd142d5fa0f2bc82636f991ba
tefa766 <sanchez.lizbeth.2024@cbtis-246.edu.mx>
2025-03-03 17:25:07 -0600

    Add files via upload

---
+++ Practica1.py
@@ -0,0 +1,23 @@
+enero = 31
+febrero = 28
+marzo = 31
+abril = 30 
+mayo = 31 
+junio = 30  
+julio = 31 
+agosto = 31 
+septiembre = 30
+octubre = 31
+noviembre = 30 
+diciembre = 31 
+dia_nac = 7
+mes_nac = diciembre
+año_nac = 2009
+
+dia_hoy = 10 
+mes_hoy = febrero
+año_actual = 2025
+
+print(2025-2009)
+
+
