69b7eebb4a9e5fd81a025621ee546ff6da61b230
tefa766 <sanchez.lizbeth.2024@cbtis-246.edu.mx>
2025-03-03 17:24:12 -0600

    Add files via upload

---
+++ estefania.py
@@ -0,0 +1,32 @@
+enero = 31
+febrero = 28
+marzo = 31
+abril = 30 
+mayo = 31 
+junio = 30  
+julio = 31 
+agosto = 31 
+septiembre = 30
+octubre = 31
+noviembre = 30 
+diciembre = 31 
+dia_nac = 7
+mes_nac = octubre
+año_nac = 2007
+
+dia_hoy = 10 
+mes_hoy = febrero
+año_actual = 2025
+
+edad=año_actual-año_nac
+dias_vividos_2025=enero+dia_hoy
+dias=365
+dias_año_nac=diciembre-dia_nac
+
+print((edad*dias)+dias_vividos_2025+dias_año_nac)
+
+
+
+
+
+
