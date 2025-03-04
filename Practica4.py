e846ef5d94ca3516dc37fb2411fca64ba4a1701f
tefa766 <sanchez.lizbeth.2024@cbtis-246.edu.mx>
2025-03-04 14:16:01 -0600

    Add files via upload

---
+++ Practica4.py
@@ -0,0 +1,14 @@
+#Codificar una solucion que escriba el siguiente
+#texto llenando los espacios en blanco
+
+#Mi nombre es ______ y tengo ____ años de edad.
+#Tambien, Siempre he querido ser ________
+#Gracias por leer me!
+
+nombre = input("Cual es tu nombre")
+edad = input("Que edad tienes")
+profesion = input("Que deseas estudiar")
+
+print("Mi nombre es",nombre,"y tengo",edad,"años de edad.")
+print("Tambien, Siempre he querido ser",profesion)
+print("Gracias por leer me!")
